72c9430a39032654bf3a017aebd634e466dff322
Gudulsson <ludvigschonbeck@gmail.com>
2025-06-19 16:15:21 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# Twitter API
+TWITTER_BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAC9k2gEAAAAAtmW8w5lfmaN3v%2FGMkLS3Vixr3dM%3Dth7FwbtUmh5r0AskdRN3OqMahtfizpHNvDYvYkrlTRL3HVLapo"
+
+# OpenAI
+OPENAI_API_KEY = "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"
+
+# SMTP / Mail
+EMAIL_ADDRESS = "ludvigschonbeck@gmail.com"
+EMAIL_PASSWORD = "fhol whoc rtdy vhbf"
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+
+# Övrigt
+RECIPIENT_EMAIL = "ludvigschonbeck@gmail.com"
